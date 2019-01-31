c87c6ce8be85dec8b98bf065161d878215da93ec
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-31 20:34:32 +0100

    solidifying knowledge

--- tablePrinter.py
+++ tablePrinter.py
@@ -13,7 +13,6 @@ def findLongestString(table):
                     colWidth[x] = len(table[x][y])
     return colWidth
             
-            
 def printTableJustifiedToLongestStringInRow(table):
     cols = len(tableData[0])
     rows = len(tableData)
@@ -29,4 +28,39 @@ printTableJustifiedToLongestStringInRow(tableData)
 def findLongestString(table):
     colsWidth = [0] *len(table)
     cols = len(table[0])
+    rows = len(table)
+
+    for x in range(cols):
+        for y in range(rows):
+            if colsWidth[y] < len(table[y][x]):
+                colsWidth[y] = len(table[y][x])
+    return colsWidth
+
+def printTable(table):
+    colsWidth = findLongestString(tableData)
+    rows = len(table)
+    cols = len(table[0])
+
+    for x in range(cols):
+        for y in 
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
